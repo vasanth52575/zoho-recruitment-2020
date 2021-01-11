021f4c4d9356fb5c3bbc714b0a91c0c39055dc09
vasanth52575 <77257466+vasanth52575@users.noreply.github.com>
2021-01-11 09:34:10 +0530

    Add files via upload

---
+++ pattern-zoho-recruitment-2020.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,k;
+    cin>>a;
+    for(int i=1;i<=a;i++){
+        for(int j=a;j>a-i;j--){
+            cout<<j;
+        }
+        for(int j=1;j<=a-i;j++){
+            cout<<(a-i+1);
+        }
+        cout<<endl;
+    }
+} // namespace std;
+
